56c59a97043caaa04f980f13232fe221f3afefae
sergt <slope177@calpoly.edu>
2024-11-11 15:47:39 -0800

    Push Work Please

--- convert.py
+++ convert.py
@@ -1,1 +1,9 @@
-
+#converts a string to a float value
+def str_to_float(string:str) -> float:
+    try:
+        #trys to see if it makes conversion
+        conversion = float(string)
+        return conversion
+    #Exception it was incable of translating the value and returns None
+    except ValueError:
+        return None
--- convert_tests.py
+++ convert_tests.py
@@ -1,1 +1,15 @@
+import convert
+import unittest
+
+class Lab7unittests(unittest.TestCase):
+
+    def test_1(self):
+        result = convert.str_to_float("201234124")
+        check = 201234124
+        self.assertEqual(check, result)
+
+    def test_2(self):
+        result = convert.str_to_float("Hello")
+        check = None
+        self.assertEqual
 
