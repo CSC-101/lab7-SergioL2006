9c4abad796c073ec8df99e063db0f9d6ce889e1d
sergt <slope177@calpoly.edu>
2024-11-11 23:49:03 -0800

    Figured out the idea to Command Line DONE

--- command_line.py
+++ command_line.py
@@ -1,4 +1,18 @@
 import sys
+from convert import str_to_float
+
+def main():
+    #placeholder for total
+    total = 0
+    #begins at first index to end
+    for arg in sys.argv[1:]:
+        #used to check the inputs if it is a string or not
+        number = str_to_float(arg)
+        #if the value is a float value, it will add on
+        if number == float:
+            total += number
+    #prints total values added upon one another
+    print("The values sum is: ", total)
 
 if __name__ == '__main__':
-    print(sys.argv)
+    main()
